fb0969a9b5437a9e5b792a5492298b1e814dae9e
MayraMendoza <mayra601m@gmail.com>
2022-06-11 23:54:09 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,116 @@
+# SET 1
+#
+# #1
+# Return the First Element in an Array (getfirstelement.js)
+# https://edabit.com/challenge/QaApgtePE6QrCZ64o
+#
+# #2
+# Return the Next Number from the Integer Passed (nextnumber.js)
+# https://edabit.com/challenge/NAQhEoxbofPidLxm9
+#
+# #3
+# Find the Index (findtheindex.js)
+# https://edabit.com/challenge/z6Pxiw289JtaE2ndL
+#
+# #4
+# Convert Minutes into Seconds (mintosec.js)
+# https://edabit.com/challenge/8q54MKnRrm89pSLmW
+#
+# #5
+# Return Something to Me! (returnsomthing.js)
+# https://edabit.com/challenge/MvZK536X7fyrWH8Qc
+#
+# SET 2
+#
+# #6
+# Flip the Boolean
+# https://edabit.com/challenge/KWRQP86jxzM9SZmRM
+#
+# #7
+# Get Word Count
+# https://edabit.com/challenge/5LnycSd2xT4uwZCpi
+#
+# #8
+# Exists a Number Higher?
+# https://edabit.com/challenge/aWzxPLibBLJgn8AbZ
+#
+# #9
+# How Many Vowels?
+# https://edabit.com/challenge/GBKphScsmDi9ek3ra
+# SET 2 BONUS
+# Reverse the Number
+# https://edabit.com/challenge/tiyPFonvAJ8e6H9jS
+#
+# Video Length
+# https://edabit.com/challenge/9D39q2q9yAFCDM9da
+#
+# Difference of Max and Min
+# https://edabit.com/challenge/hymPkXdhmDQLe87QT
+#
+# Double Letters
+# https://edabit.com/challenge/EaWY5d2pYBckrkAnS
+#
+# Sort Numbers
+# https://edabit.com/challenge/WM5s7vuHnXdcKCEjS
+#
+# SET 3
+#
+# #10
+# Return the Sum of Two Numbers
+# https://edabit.com/challenge/8Ty2vq2YtcBp6CFc8
+#
+# #11
+# Are the Numbers Equal?
+# https://edabit.com/challenge/5gpukjfi5rAH8MvKF
+#
+# #12
+# Convert Age to Days
+# https://edabit.com/challenge/eqcCH2WbEXFLDyiQi
+#
+# #13
+# Name Greeting!
+# https://edabit.com/challenge/JAtN6KLtahAkmT3n3
+#
+# #14
+# Reverse the Order of a String
+# https://edabit.com/challenge/5gPCp7v7iDWZvb4YQ
+#
+# SET 3 BONUS
+#
+# Area of a Triangle
+# https://edabit.com/challenge/B4HjZbBh9yuELmjna
+#
+# Multiple of 100
+# https://edabit.com/challenge/KnRj8TSD63JBpzDfi
+#
+# Capture the Rook
+# https://edabit.com/challenge/rYD9NTBmNhaPM6wx2
+#
+#
+#
+# SET 4
+# #15
+# Two Makes Ten
+# https://edabit.com/challenge/Nz6AzC4v8jyMCH2Tu
+#
+#
+# #16
+# Count Syllables
+# https://edabit.com/challenge/HDWqr2HdC3dwHYeBx
+#
+#
+#
+# #17
+# Case Insensitive Comparison
+# https://edabit.com/challenge/D6Lcut2s2gEzdCPvv
+#
+#
+# #18
+# H4ck3r Sp34k
+# https://edabit.com/challenge/wFPAkqT72h4WGEuYN
+#
+#
+#
+# BONUS: Store each algorithm in a new class.
+# Use the NameGenerator class we created for JavaBasics 3/3 as an example. https://github.com/mguadagni/JavaBasics/tree/master/src/com/careerdevs
+#
